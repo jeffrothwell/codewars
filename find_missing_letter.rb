caa598a671676e86121e3ae4cad397d031fa8a30
jeffrothwell <rothwelljeff@gmail.com>
2018-04-13 14:32:13 -0400

    complete exercise

--- find_missing_letter.rb
+++ find_missing_letter.rb
@@ -1,3 +1,9 @@
 def find_missing_letter(arr)
-  #your code here
+  arr.each_with_index do |letter, i|
+    if arr[i + 1].ord - letter.ord > 1
+      return (letter.ord + 1).chr("UTF-8")
+    end
+  end
 end
+
+puts find_missing_letter(["a", "b", "d"])
