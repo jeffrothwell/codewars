218f23378c17af821a47260e64e574295fcc71f6
jeffrothwell <rothwelljeff@gmail.com>
2018-05-13 22:56:19 -0400

    produce sorted hash of letter, frequency and origin array data

--- string_mix_summary.rb
+++ string_mix_summary.rb
@@ -1,11 +1,34 @@
 def mix(s1, s2)
-    s1_frequency_hash = {}
-    s2_frequency_hash = {}
-    s1.split(//).each do |char|
-      if /[a-z]/.match(char)
-        puts char
+  s1_freqs = freq_hash(s1)
+  s2_freqs = freq_hash(s2)
+  output_string_array = []
+  commmon_letters_hash = {}
+  s1_freqs.each do |key, value|
+    if s2_freqs.keys.include?(key)
+      if s2_freqs[key] > value
+        commmon_letters_hash[key] = {"2" => s2_freqs[key]}
+      elsif s2_freqs[key] < value
+        commmon_letters_hash[key] = {"1" => value}
+      else
+        commmon_letters_hash[key] = {"=" => value}
       end
     end
+  end
+  puts commmon_letters_hash.sort_by{ |_key, value| value.values.first }.reverse.to_h
+end
+
+def freq_hash(hash)
+  frequency_hash = {}
+  hash.split(//).each do |char|
+    if /[a-z]/.match(char)
+      if frequency_hash[char]
+        frequency_hash[char] += 1
+      else
+        frequency_hash[char] = 1
+      end
+    end
+  end
+  frequency_hash
 end
 
 s1 = "my&friend&Paul has heavy hats! &"
